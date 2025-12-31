63781ddb258b6a905e1b3aea3c3077178762bdb7
mrgentil <tshitshob@gmail.com.com>
2025-12-31 07:56:38 +0100

    fixed error prod

--- backend/src/company/company.module.ts
+++ backend/src/company/company.module.ts
@@ -3,9 +3,10 @@ import { CompanyService } from './company.service';
 import { CompanyController } from './company.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { RolesModule } from '../roles/roles.module';
 
 @Module({
-    imports: [PrismaModule, AuthModule],
+    imports: [PrismaModule, AuthModule, RolesModule],
     controllers: [CompanyController],
     providers: [CompanyService],
     exports: [CompanyService],
