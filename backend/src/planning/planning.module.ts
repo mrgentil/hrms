632f95e78b6e70a6f430ee4ad9736d20d4c12620
mrgentil <tshitshob@gmail.com.com>
2026-06-25 16:10:03 +0100

    fix(planning): import RolesModule to fix PermissionsGuard dependency injection

--- backend/src/planning/planning.module.ts
+++ backend/src/planning/planning.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlanningService } from './planning.service';
 import { PlanningController } from './planning.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { RolesModule } from '../roles/roles.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RolesModule],
   controllers: [PlanningController],
   providers: [PlanningService],
 })
